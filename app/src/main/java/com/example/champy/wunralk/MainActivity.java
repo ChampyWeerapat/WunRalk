0b2162e30af080b9c8eae7360d5fb49850aecccd
5610404606 <weerapat.ka@ku.th>
2015-10-13 10:42:43 +0700

    create Project

---
+++ app/src/main/java/com/example/champy/wunralk/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.champy.wunralk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Runtime runtime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView duration = (TextView) findViewById(R.id.time_duration);
+        runtime = new Runtime(duration);
+
+        final ImageButton clickHereBtn = (ImageButton)findViewById(R.id.startstopbtn);
+        clickHereBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickHereBtn.setSelected(!clickHereBtn.isSelected());
+                if (clickHereBtn.isSelected()) {
+                    runtime.start();
+                    clickHereBtn.setBackgroundResource(R.drawable.stop);
+
+                } else {
+                    runtime.pause();
+                    clickHereBtn.setBackgroundResource(R.drawable.start);
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/champy/wunralk/Runtime.java
@@ -0,0 +1,54 @@
+package com.example.champy.wunralk;
+
+import android.os.Handler;
+import android.os.SystemClock;
+import android.widget.TextView;
+
+/**
+ * Created by kasem on 13/10/2558.
+ */
+public class Runtime implements Runnable {
+    long timeMilli = 0L;
+    long timeStart = 0L;
+    long finaltime = 0L;
+    long timeSwap = 0L;
+    Handler myHandler;
+    TextView text;
+
+    public Runtime(TextView textView){
+        text = textView;
+        myHandler = new Handler();
+    }
+    @Override
+    public void run() {
+        timeMilli = SystemClock.uptimeMillis() -timeStart;
+        finaltime = timeSwap + timeMilli;
+
+        int seconds = (int) (finaltime / 1000);
+        int minutes = seconds / 60;
+        seconds = seconds % 60;
+        int milliseconds = (int) (finaltime % 1000);
+        int hours = minutes/60;
+        text.setText(String.format("%02d:%02d:%02d:%03d",hours,minutes,seconds,milliseconds));
+        myHandler.postDelayed(this, 0);
+    }
+
+    public  void pause(){
+        timeSwap += timeMilli;
+        myHandler.removeCallbacks(this);
+    }
+
+    public void start(){
+        timeStart = SystemClock.uptimeMillis();
+        myHandler.postDelayed(this, 0);
+    }
+
+    public String getTime(){
+        int seconds = (int) (finaltime / 1000);
+        int minutes = seconds / 60;
+        seconds = seconds % 60;
+        int milliseconds = (int) (finaltime % 1000);
+        int hours = minutes/60;
+        return (String.format("%02d:%02d:%02d:%03d",hours,minutes,seconds,milliseconds));
+    }
+}
